da18645a9540f941ce0969fa5b1038aff72f1d34
Kakande122222 <developerfrankproug@gmail.com>
2024-12-04 00:22:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ global.github = process.env.GITHUB || "https://github.com/Toxic1239/RIASGREMORYB
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vaex2BtGU3BRQoeEsl0U";
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vaex2BtGU3BRQoeEsl0U";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/c21ae479aab857b4f8006.jpg";
-global.devs = "2348086541281";
-global.sudo = process.env.SUDO || "2348086541281";
-global.owner = process.env.OWNER_NUMBER || "2348086541281";
+global.devs = "256765050916";
+global.sudo = process.env.SUDO || "256765050916";
+global.owner = process.env.OWNER_NUMBER || "256765050916";
 global.style = process.env.STYLE || "1";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,13 +42,13 @@ global.scan = "https://riasgremorybot-xcqv.onrender.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSU1pU01EcXgvaVdlSDgvMHgyZXgxZVlyeEtudXpqOUFXK2ZRVlR2ZWprOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicU1iRVJ5R1ZOR2JnYml1anA5M0xIc25YeThvZmNxaUpRcUJTT09iSnNqST0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ1TGE2aFJYSGNVVUJHQXdsa2JCRWJ2aDJGM0UxZ3MrNUMybllqK1V0YTFzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJWHNpRzNWNnlRcUo3d3RUeU9tdEV3cEl6WVZwSVJ6SzBtcmQyTEJOZFJZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFBVG85cmV4MUpVUkJxdGFYKzVBT25SQ1lSR1BHak8wRjVzTDgwUlQzbnM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjFLNUR0OEl4bE1vTnBFMGF4WTdBZU5ReFdJM0hCU2ovWS9rOWtZU0x0QTg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0MyR0pxNEtHL05yc09zb3FMdHFYSDdvY1Uxdm5jYy9QRGtoUzUzaklGbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWm56V1A1KzFWaWpoRnkxOUFhZXdDY0RsYS9JTDlaVjkrUUNjQWxqVnBHaz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJjdUV3V0VwL1NSSHAwbTR6RjFleTRMQjFqN1BrWGU2cERzZUY1Z042NTBZV2IzNEZjMU92Q2dKeWNqRWZlQitjMWNneFQzaUtUUWJBU2ZmdmpxQWlBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6ODAsImFkdlNlY3JldEtleSI6ImdibGhEcWREY2lkMU15Z1MvbUoxS0lIWnZYMzB6eDQxWVhOcm8xWGhGc2s9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImhMRXlfbFRXVFFPX0V1QjlCUWVlSEEiLCJwaG9uZUlkIjoiZWM4YTJiMjktYTkyNy00NGU4LWEzZjYtMzQ0NTZiMWE3ZGI3IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkViUWR3TnJSWUxtOEtzYzc3em9FbFFQRVJPYz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIyaDRURTRTL3NKVVM1Rko4MG13NVF0RnB6aUE9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiRExNVlFHQlQiLCJtZSI6eyJpZCI6IjI1Njc2NTA1MDkxNjo1OEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDS0NEak80R0VNN3J2Ym9HR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiYkF2Q2g4b0h2MUgrTlMzVjgrZ0JsZ1pidjdPQ3RQdW4yRUMvRTRHRVN6QT0iLCJhY2NvdW50U2lnbmF0dXJlIjoibWFxazY1VUI2QkJKeEplcWRSK01hK0NETHljV01SdWFuR1FRNXl1RVhnNnNlRDdic1o4cURDQ2pzWlo0L0JZUWtuaS9URXhFdmFZRnVoeGFxdllDQ1E9PSIsImRldmljZVNpZ25hdHVyZSI6IkEvYUZkY0tVZ250bDNwQnJkcGJENUVhUXhWYSsySTUxdFRaMTFmWDBxWUNPNURWRXdWVWJUcWdjUTRYRkVUOEFHUDhDeUk5dWJYT014cG9RZVZYNGhnPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU2NzY1MDUwOTE2OjU4QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQld3THdvZktCNzlSL2pVdDFmUG9BWllHVzcremdyVDdwOWhBdnhPQmhFc3cifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MzMyNjA3NjJ9"
 module.exports = {
   menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.3.1",
-  caption: process.env.CAPTION || "𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗧𝗢𝗫𝗫𝗜𝗖 𝗕𝗢𝗬",
+  caption: process.env.CAPTION || "𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗙𝗥𝗔𝗡𝗞",
   author: process.env.PACK_AUTHER || "ᴛᴏxxɪᴄ ᴍᴅ",
   packname: process.env.PACK_NAME || "𝗥𝗜𝗔𝗦 𝗠𝗗",
   botname: process.env.BOT_NAME || "𝗥𝗜𝗔𝗦 𝗚𝗥𝗘𝗠𝗢𝗥𝗬",
